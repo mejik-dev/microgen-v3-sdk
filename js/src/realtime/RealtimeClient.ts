548b6cf7ce54e981a9b0bf1a9b30a4fb1b029d8d
drScripts <nathanael.vd@gmail.com>
2023-09-06 18:30:29 +0700

    fix: stringify options

--- js/src/realtime/RealtimeClient.ts
+++ js/src/realtime/RealtimeClient.ts
@@ -58,7 +58,7 @@ export default class RealtimeClient {
         let filter = '';
 
         if (where) {
-          filter = ':' + qs.stringify(where, { encode: false });
+          filter = ':' + qs.stringify(where, { encodeValuesOnly: true });
         }
 
         const channel = `${getChannel.data.name}:${event || '*'}${filter}`;
