f66336cdf5ad3de9aa80a3b1a1ecb3810d386659
Muhamad Chairul Aziz <m.chairul669@gmail.com>
2024-02-20 11:41:37 +0700

    fix: register input

--- js/src/auth/AuthClient.ts
+++ js/src/auth/AuthClient.ts
@@ -92,13 +92,7 @@ export default class AuthClient {
     });
   }
 
-  async register<T = any>(body: {
-    firstName: string;
-    lastName?: string;
-    email: string;
-    password: string;
-    role?: string;
-  }): Promise<AuthResponse<T>> {
+  async register<T = any>(body: Partial<T>): Promise<AuthResponse<T>> {
     return new Promise(async (resolve, _reject) => {
       try {
         const httpsAgent = await hA();
