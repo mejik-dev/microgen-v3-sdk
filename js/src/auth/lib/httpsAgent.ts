f5c9b5ba014b6c107f6cd02716585609681d1e84
Egi Ginting <exneval.rayz@gmail.com>
2024-04-14 14:39:32 +0700

    feat: support nextjs edge runtime
    Signed-off-by: Egi Ginting <exneval.rayz@gmail.com>

--- js/src/auth/lib/httpsAgent.ts
+++ js/src/auth/lib/httpsAgent.ts
@@ -1,5 +1,9 @@
 export default async function httpsAgent() {
   if (typeof window === 'undefined') {
+    if (process.env.NEXT_RUNTIME === 'edge') {
+      return undefined;
+    }
+
     return new (await import('https')).Agent({ rejectUnauthorized: false });
   }
 
